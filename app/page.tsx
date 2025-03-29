56934205411685ea8ec3cc63ad74977b132e0494
conormendenhall <conor.mendenhall@gmail.com>
2025-03-29 00:01:26 -0400

    Layout, more data

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { Jacquard_24 } from "next/font/google";
 import {
-  Names,
   Backgrounds,
   Physiques,
   Skins,
@@ -22,12 +21,18 @@ const jacquard = Jacquard_24({
 });
 
 export default function Home() {
-  const [name, setName] = useState<string | null>(null);
   // Abilities
   const [hitProtection, setHitProtection] = useState<number | null>(null);
   const [strength, setStrength] = useState<number | null>(null);
   const [dexterity, setDexterity] = useState<number | null>(null);
   const [willpower, setWillpower] = useState<number | null>(null);
+  // Background
+  const [background, setBackground] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
+  const [historyLabel, setHistoryLabel] = useState<string | null>(null);
+  const [history, setHistory] = useState<string | null>(null);
+  const [specialLabel, setSpecialLabel] = useState<string | null>(null);
+  const [special, setSpecial] = useState<string | null>(null);
   // Traits
   const [physique, setPhysique] = useState<string | null>(null);
   const [face, setFace] = useState<string | null>(null);
@@ -37,22 +42,44 @@ export default function Home() {
   const [virtue, setVirtue] = useState<string | null>(null);
   const [vice, setVice] = useState<string | null>(null);
   const [speech, setSpeech] = useState<string | null>(null);
-  const [background, setBackground] = useState<string | null>(null);
   const [age, setAge] = useState<number | null>(null);
+  // Possessions
+  const [gold, setGold] = useState<number | null>(null);
 
   const rollDie = (sides: number): number => {
     return Math.floor(Math.random() * sides) + 1;
-  };
-
-  const generateName = (): void => {
-    const roll = Math.floor(Math.random() * Names.length);
-    setName(Names[roll]);
   };
 
   const rollHitProtection = (): void => setHitProtection(rollDie(6));
   const rollStrength = (): void => setStrength(rollDie(6));
   const rollDexterity = (): void => setDexterity(rollDie(6));
   const rollWillpower = (): void => setWillpower(rollDie(6));
+
+  const rollBackground = (): void => {
+    const background =
+      Backgrounds[Math.floor(Math.random() * Backgrounds.length)];
+    setBackground(background.Name);
+    setName(
+      background.Names[Math.floor(Math.random() * background.Names.length)]
+    );
+    setHistoryLabel(background.HistoryLabel);
+    setHistory(
+      background.Histories[
+        Math.floor(Math.random() * background.Histories.length)
+      ]
+    );
+    setSpecialLabel(background.SpecialLabel ?? null);
+    setSpecial(
+      background.Specials[
+        Math.floor(Math.random() * background.Specials.length)
+      ] ?? null
+    );
+    let gold = 0;
+    for (let i = 0; i < background.GoldDice[0]; i++) {
+      gold += rollDie(background.GoldDice[1]);
+    }
+    setGold(gold);
+  };
 
   const rollPhysique = (): void => {
     const roll = Math.floor(Math.random() * Physiques.length);
@@ -94,131 +121,176 @@ export default function Home() {
     setSpeech(Speech[roll]);
   };
 
-  const rollBackground = (): void => {
-    const roll = Math.floor(Math.random() * Backgrounds.length);
-    setBackground(Backgrounds[roll]);
-  };
-
   const rollAge = (): void => {
     setAge(rollDie(20) + rollDie(20) + 10);
   };
 
   return (
-    <div className="min-h-screen max-w-[30rem] m-auto items-center">
-      <section>
-        <p className="m-4 text-[3rem]">{name}</p>
-        <div className="grid gap-2">
+    <div className="min-h-screen m-2">
+      <div className="grid sm:grid-cols-2 gap-2 items-start sm:m-auto sm:max-w-[48rem]">
+        <h1 className="text-[3rem] sm:col-span-2">{name}</h1>
+        <section className="grid gap-2">
           <div
             id="abilities"
             className="grid grid-flow-row grid-cols-4 items-center gap-2"
           >
-            <div>
-              <button onClick={() => rollHitProtection()}>HP</button>
-              <p>{hitProtection && `${hitProtection}`}</p>
-            </div>
-            <div>
-              <button onClick={() => rollStrength()}>STR</button>
-              <p>{strength && `${strength}`}</p>
-            </div>
-            <div>
-              <button onClick={() => rollDexterity()}>DEX</button>
-              <p>{dexterity && `${dexterity}`}</p>
-            </div>
-            <div>
-              <button onClick={() => rollWillpower()}>WIL</button>
-              <p>{willpower && `${willpower}`}</p>
-            </div>
+            {hitProtection && (
+              <div>
+                <button onClick={() => rollHitProtection()}>HP</button>
+                <p>{hitProtection && `${hitProtection}`}</p>
+              </div>
+            )}
+            {strength && (
+              <div>
+                <button onClick={() => rollStrength()}>STR</button>
+                <p>{strength && `${strength}`}</p>
+              </div>
+            )}
+            {dexterity && (
+              <div>
+                <button onClick={() => rollDexterity()}>DEX</button>
+                <p>{dexterity && `${dexterity}`}</p>
+              </div>
+            )}
+            {willpower && (
+              <div>
+                <button onClick={() => rollWillpower()}>WIL</button>
+                <p>{willpower && `${willpower}`}</p>
+              </div>
+            )}
           </div>
-          <div className="flex bg-[--background-b]">
-            <button
-              className="w-1/2 p-2 text-left"
-              onClick={() => rollBackground()}
-            >
-              Background
-            </button>
-            <p className="p-2">{background}</p>
-          </div>
-        </div>
-      </section>
-      <section id="traits" className="grid gap-2">
-        <h2>Traits</h2>
-        <div>
-          <button onClick={() => rollPhysique()} className="w-1/2 text-left">
-            Physique
+          {background && (
+            <div className="flex bg-[--background-b]">
+              <button
+                className="w-1/2 p-2 text-left"
+                onClick={() => rollBackground()}
+              >
+                Background
+              </button>
+              <p className="p-2">{background}</p>
+            </div>
+          )}
+          {history && (
+            <div className="bg-[--background-b]">
+              <p className="p-2 primary font-bold">{historyLabel}</p>
+              <p className="p-2">{history}</p>
+            </div>
+          )}
+          {special && (
+            <div className="bg-[--background-b]">
+              <p className="p-2 primary font-bold">{specialLabel}</p>
+              <p className="p-2">{special}</p>
+            </div>
+          )}
+          {gold && (
+            <div className="bg-[--background-b]">
+              <p className="p-2 primary font-bold">Gold</p>
+              <p className="p-2">{gold}</p>
+            </div>
+          )}
+        </section>
+        <section id="traits" className="grid gap-2">
+          {physique && (
+            <div>
+              <button
+                onClick={() => rollPhysique()}
+                className="w-1/2 text-left"
+              >
+                Physique
+              </button>
+              <p className="px-4 py-2">{physique}</p>
+            </div>
+          )}
+          {skin && (
+            <div>
+              <button onClick={() => rollSkin()} className="w-1/2 text-left">
+                Skin
+              </button>
+              <p className="px-4 py-2">{skin}</p>
+            </div>
+          )}
+          {hair && (
+            <div>
+              <button onClick={() => rollHair()} className="w-1/2 text-left">
+                Hair
+              </button>
+              <p className="px-4 py-2">{hair}</p>
+            </div>
+          )}
+          {face && (
+            <div>
+              <button onClick={() => rollFace()} className="w-1/2 text-left">
+                Face
+              </button>
+              <p className="px-4 py-2">{face}</p>
+            </div>
+          )}
+          {speech && (
+            <div>
+              <button onClick={() => rollSpeech()} className="w-1/2 text-left">
+                Speech
+              </button>
+              <p className="px-4 py-2">{speech}</p>
+            </div>
+          )}
+          {clothing && (
+            <div>
+              <button
+                onClick={() => rollClothing()}
+                className="w-1/2 text-left"
+              >
+                Clothing
+              </button>
+              <p className="px-4 py-2">{clothing}</p>
+            </div>
+          )}
+          {virtue && (
+            <div>
+              <button onClick={() => rollVirtue()} className="w-1/2 text-left">
+                Virtue
+              </button>
+              <p className="px-4 py-2">{virtue}</p>
+            </div>
+          )}
+          {vice && (
+            <div>
+              <button onClick={() => rollVice()} className="w-1/2 text-left">
+                Vice
+              </button>
+              <p className="px-4 py-2">{vice}</p>
+            </div>
+          )}
+          {age && (
+            <div>
+              <button onClick={() => rollAge()} className="w-1/2 text-left">
+                Age
+              </button>
+              <p className="px-4 py-2">{age}</p>
+            </div>
+          )}
+          <button
+            className={`${jacquard.className} text-[3rem]`}
+            onClick={() => {
+              rollBackground();
+              rollHitProtection();
+              rollStrength();
+              rollDexterity();
+              rollWillpower();
+              rollPhysique();
+              rollSkin();
+              rollHair();
+              rollFace();
+              rollSpeech();
+              rollClothing();
+              rollVirtue();
+              rollVice();
+              rollAge();
+            }}
+          >
+            Randomize
           </button>
-          <p className="px-4 py-2">{physique}</p>
-        </div>
-        <div>
-          <button onClick={() => rollSkin()} className="w-1/2 text-left">
-            Skin
-          </button>
-          <p className="px-4 py-2">{skin}</p>
-        </div>
-        <div>
-          <button onClick={() => rollHair()} className="w-1/2 text-left">
-            Hair
-          </button>
-          <p className="px-4 py-2">{hair}</p>
-        </div>
-        <div>
-          <button onClick={() => rollFace()} className="w-1/2 text-left">
-            Face
-          </button>
-          <p className="px-4 py-2">{face}</p>
-        </div>
-        <div>
-          <button onClick={() => rollSpeech()} className="w-1/2 text-left">
-            Speech
-          </button>
-          <p className="px-4 py-2">{speech}</p>
-        </div>
-        <div>
-          <button onClick={() => rollClothing()} className="w-1/2 text-left">
-            Clothing
-          </button>
-          <p className="px-4 py-2">{clothing}</p>
-        </div>
-        <div>
-          <button onClick={() => rollVirtue()} className="w-1/2 text-left">
-            Virtue
-          </button>
-          <p className="px-4 py-2">{virtue}</p>
-        </div>
-        <div>
-          <button onClick={() => rollVice()} className="w-1/2 text-left">
-            Vice
-          </button>
-          <p className="px-4 py-2">{vice}</p>
-        </div>
-        <div>
-          <button onClick={() => rollAge()} className="w-1/2 text-left">
-            Age
-          </button>
-          <p className="px-4 py-2">{age}</p>
-        </div>
-        <button
-          className={`${jacquard.className} text-[3rem]`}
-          onClick={() => {
-            generateName();
-            rollHitProtection();
-            rollStrength();
-            rollDexterity();
-            rollWillpower();
-            rollBackground();
-            rollPhysique();
-            rollSkin();
-            rollHair();
-            rollFace();
-            rollSpeech();
-            rollClothing();
-            rollVirtue();
-            rollVice();
-            rollAge();
-          }}
-        >
-          Randomize
-        </button>
-      </section>
+        </section>
+      </div>
     </div>
   );
 }
